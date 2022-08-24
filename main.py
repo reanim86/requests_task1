075b2301f978a9fd0a5b6eb1b9d47605a32f333f
Aleksei Kudinov <reanim86@yandex.ru>
2022-08-24 12:01:03 +0300

    Add 3 function

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import requests
-from pprint import pprint
-url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json'
-response = requests.get(url)
-superhero = response.json()
-pprint(superhero)
+
+def get_data():
+    url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json'
+    response = requests.get(url)
+    superhero = response.json()
+    return superhero
+
+def searc_hero(hero_list):
+    hulk = {}
+    captain_america = {}
+    thanos = {}
+    for hero in hero_list:
+        if hero['name'] == 'Hulk':
+            hulk = hero
+        elif hero['name'] == 'Captain America':
+            captain_america = hero
+        elif hero['name'] == 'Thanos':
+            thanos = hero
+    list_hero = (hulk, captain_america, thanos)
+    return list_hero
+
+def search_intelligence_hero(heroes):
+    if heroes[0]['powerstats']['intelligence'] > heroes[1]['powerstats']['intelligence']:
+        if heroes[0]['powerstats']['intelligence'] > heroes[2]['powerstats']['intelligence']:
+            print(f'Самый умный {heroes[0]["name"]}')
+        else:
+            print(f'Самый умный {heroes[2]["name"]}')
+    else:
+        if heroes[1]['powerstats']['intelligence'] > heroes[2]['powerstats']['intelligence']:
+            print(f'Самый умный {heroes[1]["name"]}')
+        else:
+            print(f'Самый умный {heroes[2]["name"]}')
+
+if __name__ == '__main__':
+    search_intelligence_hero(searc_hero(get_data()))
+
 
